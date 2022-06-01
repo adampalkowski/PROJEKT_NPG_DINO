f9a8bce424d57568dd889a82b7178739298d4da2
szpajak <102694552+szpajak@users.noreply.github.com>
2022-06-01 22:06:58 +0200

    do wstawienia

---
+++ sound_to_import.py
@@ -0,0 +1,6 @@
+##dźwięki do wstawienia do kodu w odpowiednie miejsca
+jump_sound = pygame.mixer.Sound('resources/jump.wav')
+
+die_sound = pygame.mixer.Sound('resources/die.wav')
+
+checkPoint_sound = pygame.mixer.Sound('resources/checkPoint.wav')
