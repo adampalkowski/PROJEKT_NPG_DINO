c032db21710afc1092159bdb9ef6e28642bb6774
Adam <adampalkowski6@gmail.com>
2022-06-05 23:12:39 +0200

    update 2

--- gameplayFILE.py
+++ gameplayFILE.py
@@ -4,9 +4,8 @@ def gameplay():
     global highest_scores
     gp = 4
 
-
+    gamer_Dino = Dino(44,47)
     s_Menu = False
-
     g_Over = False
     g_exit = False
     while not g_exit:
@@ -39,9 +38,18 @@ def gameplay():
                 if event.type == pygame.QUIT:
                     g_exit = True
                     g_Over = False
-<<<<<<< HEAD
-=======
+
                 if event.type == pygame.KEYDOWN:
+                     if event.key == pygame.K_SPACE:
+                            if gamer_Dino.rect.bottom == int(0.98 * height_screen):
+                                gamer_Dino.jumping = True
+                                if pygame.mixer.get_init() != None:
+                                   #todo dodaj dzwiek skoku
+                                #todo zmien predkosc dino
+
+                    if event.type == pygame.KEYDOWN:
+                        if not (gamer_Dino.jumping and gamer_Dino.dead):
+                                gamer_Dino.ducking = True
                     if event.key == pygame.K_ESCAPE:
                         g_exit = True
                         g_Over = False
@@ -49,8 +57,17 @@ def gameplay():
                     if event.key == pygame.K_RETURN or event.key == pygame.K_SPACE:
                         g_Over = False
                         gameplay()
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_DOWN:
+                          gamer_Dino.ducking = False
 
-        highScore.update(highest_scores)
+
+
+       highScore.update(highest_scores)
+
+
+
+
         if pygame.display.get_surface() != None:
             gameover_display_message(rbtn_image, gmo_image)
             if highest_scores != 0:
