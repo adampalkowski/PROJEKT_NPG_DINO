9fac4b933080f522f8df411e659281c096f5338f
ZimnyKefir <102309076+ZimnyKefir@users.noreply.github.com>
2022-06-07 20:36:50 +0200

    Add files via upload
    Update klasy DINO

--- DINO_CLASS.py
+++ DINO_CLASS.py
@@ -2,14 +2,15 @@ class Dino():
     def __init__(self, sx=-1, sy=-1):
         #wynik dino
         self.score = 0
-
+        self.image = self.imgs[0]
         # todo : wstawic tutaj .png pliki
         self.imgs, self.rect = load_sprite_sheet()
         # todo : wstawic tutaj .png pliki
         self.imgs1, self.rect1 = load_sprite_sheet()
         self.rect.bottom = int(0.98 * height_screen)
         self.rect.left = width_screen / 15
-
+        self.index = 0
+        self.counter = 0
       #musimy wiedziec czy dino jest w skoku
         self.jumping = False
 
@@ -30,8 +31,33 @@ class Dino():
 
 #funkcja odpowiadajaca za nie wykroczenie dino poza wyznaczone pole
     def checkbounds(self):
-        pass
+        if self.rect.bottom > int(0.98 * height_screen):
+            self.rect.bottom = int(0.98 * height_screen)
+            self.jumping = False
 
-#funkcja ktora bedzie odswieżac pozycje dino oraz licznik
+    #funkcja ktora bedzie odswieżac pozycje dino oraz licznik
     def update(self):
-        pass
+
+        if self.jumping:
+            self.movement[1] = self.movement[1] + gravity
+
+        if self.jumping:
+            self.index = 0
+        elif self.blinking:
+            if self.index == 0:
+                if self.counter % 400 == 399:
+                    self.index = (self.index + 1)%2
+            else:
+                if self.counter % 20 == 19:
+                    self.index = (self.index + 1)%2
+
+
+
+        if self.dead:
+            self.index = 4
+
+
+
+
+        self.checkbounds()
+        self.counter = (self.counter + 1)
