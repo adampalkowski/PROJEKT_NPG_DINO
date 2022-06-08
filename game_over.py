f01b5e6c52f3a23781d60f3a4e28f401706a3acc
szpajak <102694552+szpajak@users.noreply.github.com>
2022-06-08 12:38:35 +0200

    dodanie komentarzy

--- game_over.py
+++ game_over.py
@@ -4,13 +4,13 @@ from pygame import *
 def gameover_display_message(rbtn_image, gmo_image):
     rbtn_rect = rbtn_image.get_rect()
 
+    #ustalenie połozenia komunikatu na ekranie
+    rbtn_rect.centerx = width_screen / 2
+    rbtn_rect.top = height_screen * 0.52
 
-rbtn_rect.centerx = width_screen / 2
-rbtn_rect.top = height_screen * 0.52
+    gmo_rect = gmo_image.get_rect()
+    gmo_rect.centerx = width_screen / 2
+    gmo_rect.centery = height_screen * 0.35
 
-gmo_rect = gmo_image.get_rect()
-gmo_rect.centerx = width_screen / 2
-gmo_rect.centery = height_screen * 0.35
-
-screen_layout_display.blit(rbtn_image, rbtn_rect)
-screen_layout_display.blit(gmo_image, gmo_rect)
+    screen_layout_display.blit(rbtn_image, rbtn_rect)
+    screen_layout_display.blit(gmo_image, gmo_rect)
