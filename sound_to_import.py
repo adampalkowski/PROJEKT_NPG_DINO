caccb7cff9ceab4facf79a9d4524e93367ab5a5e
szpajak <102694552+szpajak@users.noreply.github.com>
2022-06-08 12:57:48 +0200

    dodanie komentarzy

--- sound_to_import.py
+++ sound_to_import.py
@@ -1,6 +1,8 @@
-##dźwięki do wstawienia do kodu w odpowiednie miejsca
+#dźwięk skoku do wstawienia do kodu w odpowiednie miejsca
 jump_sound = pygame.mixer.Sound('resources/jump.wav')
 
+#dźwięk po przegranej do wstawienia do kodu w odpowiednie miejsca
 die_sound = pygame.mixer.Sound('resources/die.wav')
 
+#dźwięk uzyskania checkpointu do wstawienia do kodu w odpowiednie miejsca
 checkPoint_sound = pygame.mixer.Sound('resources/checkPoint.wav')
