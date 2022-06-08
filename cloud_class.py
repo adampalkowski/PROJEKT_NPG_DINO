fb69b361e344024c1bc44b88a14f9d4de284638e
szpajak <102694552+szpajak@users.noreply.github.com>
2022-06-08 12:36:45 +0200

    dodanie komentarzy

--- cloud_class.py
+++ cloud_class.py
@@ -1,16 +1,24 @@
 class Cloud(pygame.sprite.Sprite):
-def __init__(self,x,y):
-pygame.sprite.Sprite.__init__(self,self.containers)
-self.image,self.rect = load_image('cloud.png',int(90*30/42),30,-1)
-self.speed = 1
-self.rect.left = x
-self.rect.top = y
-self.movement = [-1*self.speed,0]
- 
-def draw(self):
-screen_layout_display.blit(self.image, self.rect)
- 
-def update(self):
-self.rect = self.rect.move(self.movement)
-if self.rect.right &lt; 0:
-self.kill()
+    def __init__(self,x,y):
+       pygame.sprite.Sprite.__init__(self,self.containers)
+
+       #TODO : wstawic w miejsce "cloud.png" odpowiednie pliki
+       self.image,self.rect = load_image('cloud.png',int(90*30/42),30,-1)
+       self.speed = 1
+       self.rect.left = x
+       self.rect.top = y
+
+       #dobór prędkości "ruchu" chmur względem ruchu dinozaura
+       self.movement = [-1*self.speed,0]
+
+    #funkcja wyświetlająca chmury na ekranie
+    def draw(self):
+       screen_layout_display.blit(self.image, self.rect)
+
+    #funkcja odświeżająca sytuacje na ekranie
+    def update(self):
+       self.rect = self.rect.move(self.movement)
+       
+       #jeśli chmura wyleci poza obszar gry zostanie "zniszczona" w kodzie programu 
+       if self.rect.right &lt; 0:
+           self.kill()
