0a6f653386700acc4706b4a36a7e33f39fe6e1fa
Placiu1 <102690951+Placiu1@users.noreply.github.com>
2022-06-14 14:03:57 +0200

    Update class_scoreboard.py

--- Dino/class_scoreboard.py
+++ Dino/class_scoreboard.py
@@ -7,17 +7,27 @@ class Scoreboard():
     def __init__(self,x=-1,y=-1):
 
         self.score = 0
+        
+        
         self.scre_img, self.screrect = load_sprite_sheet('numbers.png', 12, 1, 11, int(11 * 6 / 5), -1)
         self.image = pygame.Surface((55,int(11*6/5)))
+        
         self.rect = self.image.get_rect()
 
         if x == -1:
+            
             self.rect.left = width_screen * 0.89
+            
         else:
+            
             self.rect.left = x
+            
         if y == -1:
+            
             self.rect.top = height_screen * 0.1
+            
         else:
+            
             self.rect.top = y
             
 
@@ -33,11 +43,16 @@ class Scoreboard():
     def update(self,score):
 
         score_digits = extractDigits(score)
+        
         self.image.fill(bg_color)
 
         for s in score_digits:
+            
             self.image.blit(self.scre_img[s], self.screrect)
+            
             self.screrect.left += self.screrect.width
+            
             self.screrect.left = 0
+            
             
     #koniec klasy
